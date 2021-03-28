4f9a2e1788c23f9abcd8123e48a9b665568505f9
Rash <gizarash@gmail.com>
2021-03-28 20:58:29 +0600

    feat: implement transform function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)) 
+  {
+    throw new Error('Not array');
+  }
+
+  let result = [];
+  for(i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === 'string' && arr[i].substr(0,2) === '--') continue;
+    
+    if(arr[i-1]) {
+      if(arr[i-1] === '--discard-next') {
+        continue;
+      } else if(arr[i-1] === '--double-next') {
+        result.push(arr[i], arr[i]);
+      } else {
+        result.push(arr[i]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+
+    if(arr[i+1]) {
+      if(arr[i+1] === '--discard-prev') {
+        if(arr[i-1] && arr[i-1] === '--discard-next') {
+          continue;
+        } else {
+          result.pop()
+        }
+      } else if(arr[i+1] === '--double-prev') {
+        if(arr[i-1] && arr[i-1] === '--discard-next') {
+          continue;
+        } else {
+          result.push(arr[i]);
+        }
+      }
+    }
+  }
+  return result;
 };
