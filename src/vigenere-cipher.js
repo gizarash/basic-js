d8654f5ae5dc4d6bf3e4a1c3145d4b446836cfa3
Rash <gizarash@gmail.com>
2021-03-29 22:08:55 +0600

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,49 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isForward = true){
+    this.isForward = isForward;
+    this.initAlphabet();
+  }
+  encrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error('');
+    }
+    return this.process(message, key, true);
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if (typeof message === 'undefined' || typeof key === 'undefined') {
+      throw new Error('');
+    }
+    return this.process(message, key, false);
+  }
+  initAlphabet() {
+    this.alphabet = [];
+    const start = 65; // 'A'
+    const end = 90;   // 'Z'
+    for (let i = start; i <= end; i++) {
+        this.alphabet.push(String.fromCharCode(i));
+    }
+  }
+  process(message, key, encode) {
+    
+    message = this.isForward ? message.toUpperCase() : message.split('').reverse().join('').toUpperCase();
+    key = key.toUpperCase();
+    let count = 0;
+    return message.split('').map((e) => {
+      let result = e;
+
+      if(this.alphabet.indexOf(e) !== -1) {
+        if(encode) {
+          result = this.alphabet[(this.alphabet.indexOf(e) + this.alphabet.indexOf(key[count])) % this.alphabet.length];
+        } else {
+          result = this.alphabet[(this.alphabet.indexOf(e) + this.alphabet.length - this.alphabet.indexOf(key[count])) % this.alphabet.length];
+        }
+        count = (count + 1) % key.length;
+      }
+      
+      return result;
+    }).join('');
   }
 }
 
